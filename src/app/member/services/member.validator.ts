9f0b18cfe67c58643a9ad27588dfb94ad951b8d5
= <=>
2020-11-23 13:27:11 +0100

    canvis

--- src/app/member/services/member.validator.ts
+++ src/app/member/services/member.validator.ts
@@ -9,20 +9,19 @@ export class MemberValidator {
 
   constructor(private memberService: MemberService) { }
 
-  validateDni(control: AbstractControl){
+  validateDni(control: AbstractControl) {
 
     const dni = control.value;
     console.log(control.errors);
     let error = null;
 
-     if (control.touched || control.dirty)
-     {
-    if(this.memberService.find(dni)){
-    error = { ...error, 'duplicate':'duplicate dni' };
-    return error;
-     }
+    if (control.touched || control.dirty) {
+      if (this.memberService.find(dni)) {
+        error = { ...error, 'duplicate': 'duplicate dni' };
+        return error;
+      }
+    }
   }
-}
 
 
 }
